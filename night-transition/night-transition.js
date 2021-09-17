fba0f4380f06f082172e4803efc24804a9dd98cd
Michael Rider <mikepdxrider@gmail.com>
2021-09-16 19:33:11 -0700

    adds comments for steps for an ideal function

---
+++ night-transition/night-transition.js
@@ -0,0 +1,15 @@
+// function that:
+    // grabs bodyEl
+    // creates imgEl 
+    // applies class to element with:
+        // position absolute
+        // translateY to have it off screen
+        // z-index 500
+    // applies class to bodyEl with:
+        // fade-to-night animation
+    // applies class to imgEl with:
+        // moon-drop animation
+    //  prepend imgEl to bodyEl
+    // set a timeout equal to length of animation, inside:
+        // remove imgEl from bodyEl
+        // remove fade-to-night animation from bodyEl
